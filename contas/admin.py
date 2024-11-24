255414c502aa083b1db1f2d331f40b6c997662e2
Uendeltm <uendel_martins@outlook.com>
2024-11-23 23:38:24 -0400

    Projeto simples de aplicação com django testando url e wiews

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import categoria
+from .models import  Transacao
 
+admin.site.register(categoria)
+admin.site.register(Transacao)
 # Register your models here.
